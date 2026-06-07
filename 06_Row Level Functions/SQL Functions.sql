36178da45f05f4addcb20d57c058f9908f9a41bb
ShettiRaviTeja <shettigariraviteja@gmail.com>
2026-06-07 22:37:38 +0530

    Functions are added

---
+++ 06_Row Level Functions/SQL Functions.sql
@@ -0,0 +1,7 @@
+/*SQL Functions: It is a built-in SQL code:
+					- Accepts an input value
+					- Processes it
+					- Returns an output value
+
+Mainly we have 2 types of functions: 1. Single row functions
+									 2. Multi row functions
