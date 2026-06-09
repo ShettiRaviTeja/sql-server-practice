0d3d47370fd1eaf95f1d7c27301c08057a04468d
ShettiRaviTeja <shettigariraviteja@gmail.com>
2026-06-09 12:05:34 +0530

    Date & Time functions are au

--- 06_Row Level Functions/Single Row Functions/Date & Time Functions.sql
+++ 06_Row Level Functions/Single Row Functions/Date & Time Functions.sql
@@ -17,7 +17,7 @@ FROM Sales.Orders
 
 /* In this we have 4 categories like
 	i. Part Extraction
-	ii. Formating & Casting
+	ii. Formatting & Casting
 	iii. Calculations
 	iv. Validation */
 
@@ -89,7 +89,7 @@ DATETRUNC => DATETIME
 EOMONTH => DATE */
 
 
--- ii. Formating & Casting:
+-- ii. Formatting & Casting:
 /* We have different types of Date & Time formats:
 	International Standard (ISO 8601): YYYY - MM - DD
 	USA Standard: MM - DD - YYYY
@@ -101,7 +101,103 @@ Casting: Changing the data type from one to another. */
 
 -- Format(): Formats a date or time value.
 	-- Syntax: Format(value, format, [culture])
-SELECT
-	'Day ' + FORMAT(CreationTime, 'ddd MMM') +
-	' Q' + DATENAME(quarter, CreationTime) AS CustomFormat
+
+-- Show CreationTime using following format: Day Wed Jan Q1 2025 12:34:56 PM
+SELECT 
+	'Day ' + FORMAT(CreationTime, 'ddd MMM') + ' Q' + DATENAME(QUARTER, CreationTime) + ' ' + FORMAT(CreationTime, 'yyyy hh:mm:ss tt') Customformat
 FROM Sales.Orders
+
+-- Formatting use cases:
+-- 1. Data Aggregations:
+SELECT
+	FORMAT(CreationTime,'MMM yy') AS TimePeriod,
+	COUNT(*) AS Orders
+FROM Sales.Orders
+GROUP BY FORMAT(CreationTime,'MMM yy')
+
+-- 2. Data Standardization: In real time, we will extract the data from different sources like CSV, API, DB. We will get the different formats for same thing. At that time we should change that into a standard format using FORMAT(). Then we can use that in our analysis.
+
+
+--CONVERT():Converts a date and time value to a different data type & formats the value. Syntax: CONVERT(data_type, value [, style])
+SELECT
+	CONVERT(DATE, CreationTime) [Datetime to Date],
+	CONVERT(VARCHAR, CreationTime, 101) [USA.Std],
+	CONVERT(VARCHAR, CreationTime, 103) [UK.Std]
+FROM Sales.Orders
+
+-- CAST(): Converts a value to a specified data type. Syntax: CAST(Value AS data_type). Only used for type conversion.
+SELECT 
+CAST('123' AS INT)
+
+
+/* Comparison between CAST, FORMAT, CONVERT:
+CAST: (Value AS data_type)
+	Casting: Any type to any type
+	Formatting: No formatting 
+FORMAT: (Value, format [,culture])
+	Casting: Any type to only String
+	Formatting: Formats Date & Time, Numbers
+CONVERT: (data_type, value [,style])
+	Casting: Any type to any type
+	Formatting: Formats only Date & Time */
+
+
+-- iii. Calculations: It is used to perform mathematical operations on the date.
+/* DATEADD(): Adds or subtracts a specific time interval to/from a date.
+Syntax: (part, interval, date) */
+SELECT 
+	OrderDate [Original Date],
+	DATEADD(YEAR, 2, OrderDate) [Two Years Later],
+	DATEADD(MONTH, 3, OrderDate) [Three Months Later],
+	DATEADD(DAY, -10, OrderDate) [Ten Days before]
+FROM Sales.Orders
+
+/* DATEDIFF(): Find the difference between two dates.
+Syntax: (part, start_date, end_date) */
+SELECT 
+	DATEDIFF(YEAR, BirthDate, GETDATE()) 
+FROM Sales.Employees
+
+-- Question 1: Find the average shipping duration in days for each month.
+SELECT 
+	MONTH(OrderDate) OrderDate,
+	AVG(DATEDIFF(DAY, OrderDate, ShipDate)) AS [Avg Shipping duration]
+FROM Sales.Orders
+GROUP BY MONTH(OrderDate)
+
+-- Question 2: Find the number of days between each order and previous order.
+SELECT 
+OrderID,
+OrderDate AS CurrentOrderDate,
+LAG(OrderDate) OVER	(ORDER BY OrderDate) AS PreviousOrderDate,
+DATEDIFF(DAY, LAG(OrderDate) OVER (ORDER BY OrderDate), OrderDate) AS Noofdays
+FROM Sales.Orders
+
+
+/* iv. Validation:
+ISDATE(): Check if a value is a date. Returns 1 if the string value is a valid date.
+Syntax: ISDATE(Value) 
+It will understand only the standard format of the date and the year.
+*/
+SELECT 
+	ISDATE('123') AS Datecheck1,
+	ISDATE('2026-06-08') AS Datecheck2,
+	ISDATE('2026') AS Datecheck3,
+	ISDATE('08') AS Datecheck4,
+	ISDATE('08-06-2026') AS Datecheck5
+
+-- When we will use the ISDATE function??? Consider the following scenario:
+SELECT
+	OrderDate,
+	ISDATE(OrderDate) AS CheckDate,
+	CASE 
+		WHEN ISDATE(OrderDate) = 1 THEN CAST(OrderDate AS DATE)
+		ELSE '9999-01-01'
+	END NewOrderDate
+FROM
+(
+	SELECT '2026-06-08' AS OrderDate UNION
+	SELECT '2026-06-09' UNION
+	SELECT '2026-06-10' UNION
+	SELECT '2026-06'
+) t 
