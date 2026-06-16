4fc426180b3c72b638d10d7ef50554e26cda9fe0
ShettiRaviTeja <shettigariraviteja@gmail.com>
2026-06-16 10:40:48 +0530

    Tips are modified

--- 08_Performance Optimization/Performance Tips.sql
+++ 08_Performance Optimization/Performance Tips.sql
@@ -173,3 +173,79 @@ OPTION (HASH JOIN)
 -- Tip 18: Pre-Aggregate Data and store it in new table for reporting (CTAS or Tables)
 
 -- ===================================================================================
+-- Tip 19: JOIN vs EXISTS vs IN: When we are using another table inorder to filter one table. (Only for Filtering)
+SELECT 
+	o.OrderID,
+	o.Sales
+FROM Sales.Orders o
+INNER JOIN Sales.Customers c
+ON o.CustomerID = c.CustomerID
+WHERE c.Country = 'USA'			-- Best Practice
+
+SELECT 
+	o.OrderID,
+	o.Sales
+FROM Sales.Orders o
+WHERE EXISTS (
+			  SELECT 1
+			  FROM Sales.Customers c
+			  WHERE o.CustomerID = c.CustomerID
+			  AND Country = 'USA'
+			  )					-- Best if the Table is large
+
+SELECT o.OrderID, o.Sales
+FROM Sales.Orders o
+WHERE o.CustomerID IN (SELECT CustomerID
+					  FROM Sales.Customers c
+					  WHERE c.Country = 'USA') -- Don't use IN Operator
+
+-- ===================================================================================
+-- Tip 20: Avoid Redundant Logic in Your Query
+SELECT EmployeeID, FirstName, 'Above Average' Status 
+FROM Sales.Employees
+WHERE Salary > (SELECT AVG(Salary) FROM Sales.Employees) 
+UNION ALL
+SELECT EmployeeID, FirstName, 'Below Average' Status 
+FROM Sales.Employees
+WHERE Salary < (SELECT AVG(Salary) FROM Sales.Employees)  -- Bad Practice
+
+SELECT
+	EmployeeID,
+	FirstName,
+	CASE 
+		 WHEN Salary > AVG(Salary) OVER() THEN 'Above Average'
+		 WHEN Salary < AVG(Salary) OVER() THEN 'Below Average'
+		 ELSE 'Average'
+	END Status
+FROM Sales.Employees	-- Good Practice
+
+-- ===================================================================================
+-- Tip 21: Avoid datatypes VARCHAR & TEXT if possible
+
+-- ===================================================================================
+-- Tip 22: Avoid (MAX) unnecessarily large lengths in data types
+
+-- ===================================================================================
+-- Tip 23: Use the NOT NULL Constraint whereever applicable
+
+-- ===================================================================================
+-- Tip 24: Ensure all your tables have a Clustered Primary Key 
+
+-- ===================================================================================
+-- Tip 25: Create a Non-Clustered Index for foreign keys that are used frequently
+
+-- ===================================================================================
+-- Tip 26: Avoid Over Indexing
+
+-- ===================================================================================
+-- Tip 27: Drop Unused Indexes
+
+-- ===================================================================================
+-- Tip 28: Update Statistics (Weekly)
+
+-- ===================================================================================
+-- Tip 29: Reorganize & Rebuild Indexes (Weekly)
+
+-- ===================================================================================
+-- Tip 30: Partition Large Tables (Facts) to improve performance. 
+		-- Next Apply a Columnstore Index for the best results.
